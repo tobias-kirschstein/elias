6c30ce31d48bb17720e3cf86c3f087f932eb1300
Tobias Kirschstein <tobias.kirschstein@gmail.com>
2022-10-05 21:24:29 +0200

    Make TimeMeasurer standalone

--- src/elias/util/timing.py
+++ src/elias/util/timing.py
@@ -1,6 +1,35 @@
 from time import time
 
 
+class TimeMeasurer:
+
+    def __init__(self):
+        self.start = time()
+        self.times = []
+
+    def measure(self) -> float:
+        now = time()
+        measured_time = now - self.start
+        self.times.append(measured_time)
+        self.start = now
+        return measured_time
+
+    def print_measure(self, text: str):
+        passed_time = self.measure()
+        print(f"{text}: {passed_time:.2f}ms")
+
+    def __len__(self) -> int:
+        return len(self.times)
+
+    def __getitem__(self, i) -> float:
+        return self.times[i]
+
+    def __str__(self) -> str:
+        if len(self.times) == 1:
+            return f"{self.times[0]:.2f}"
+        return f"{self.times}"
+
+
 class Timing:
     """
     Usage:
@@ -13,32 +42,8 @@ class Timing:
     Every measurement will be available after the closure in the list t.
     """
 
-    class TimeMeasurer:
-
-        def __init__(self, start):
-            self.start = start
-            self.times = []
-
-        def measure(self) -> float:
-            now = time()
-            measured_time = now - self.start
-            self.times.append(measured_time)
-            self.start = now
-            return measured_time
-
-        def __len__(self) -> int:
-            return len(self.times)
-
-        def __getitem__(self, i) -> float:
-            return self.times[i]
-
-        def __str__(self) -> str:
-            if len(self.times) == 1:
-                return f"{self.times[0]:.2f}"
-            return f"{self.times}"
-
     def __enter__(self) -> TimeMeasurer:
-        self.time_measurer = Timing.TimeMeasurer(time())
+        self.time_measurer = Timing.TimeMeasurer()
         return self.time_measurer
 
     def __exit__(self, exc_type, exc_val, exc_tb):
